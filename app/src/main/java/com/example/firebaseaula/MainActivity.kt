c64c2a8f72f50c0bc8a10e6c7699cf09b87276b8
JonathanBritoProf <jbrito@digitalhouse.com>
2022-06-08 19:24:12 -0300

    Correção de layout e AccessViewModel.kt

--- app/src/main/java/com/example/firebaseaula/MainActivity.kt
+++ app/src/main/java/com/example/firebaseaula/MainActivity.kt
@@ -3,12 +3,38 @@ package com.example.firebaseaula
 import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.example.firebaseaula.viewmodel.AccessViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var accessViewModel : AccessViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        accessViewModel =  ViewModelProvider(this)[AccessViewModel::class.java]
+        setupListeners()
+        setupObservers()
+    }
+
+    fun setupListeners() {
+        findViewById<TextView>(R.id.txt).setOnClickListener{
+            accessViewModel.onCreateUser("a2luno123456@gmail.com","12345678")
+        }
+    }
+
+    fun setupObservers() {
+        accessViewModel.emailAuthLiveData.observe(this){
+            if (it){
+                Toast.makeText(this,"Sucesso",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Erro",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/firebaseaula/viewmodel/AccessViewModel.kt
+++ app/src/main/java/com/example/firebaseaula/viewmodel/AccessViewModel.kt
@@ -3,25 +3,29 @@ package com.example.firebaseaula.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.firebaseaula.data.UserHelper
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class AccessViewModel : ViewModel() {
 
+    var firebaseAuth = Firebase.auth
+
     var onUserRequestToRegister = MutableLiveData<Boolean>()
     var emailAuthLiveData : LiveData<Boolean> = onUserRequestToRegister
 
     fun onCreateUser (email : String , password : String) {
-       var firebaseAuth = Firebase.auth
-       var registerTask = firebaseAuth.createUserWithEmailAndPassword(email,password)
+        var registerTask = firebaseAuth.createUserWithEmailAndPassword(email,password)
+
         registerTask.addOnSuccessListener {
-           onUserRequestToRegister.value = true
-       }
+            onUserRequestToRegister.value = registerTask.isSuccessful
+        }
+
         registerTask.addOnFailureListener {
-           onUserRequestToRegister.value = false
-       }
+            onUserRequestToRegister.value = registerTask.isSuccessful
+        }
+
+
     }
-
-
 
 }
