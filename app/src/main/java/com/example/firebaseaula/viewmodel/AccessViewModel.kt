0511ff4f027236d3ba4399568a3e37f75e3eeb77
JonathanBritoProf <jbrito@digitalhouse.com>
2022-06-15 18:42:10 -0300

    Implementação realtime Database

--- app/src/main/java/com/example/firebaseaula/viewmodel/AccessViewModel.kt
+++ app/src/main/java/com/example/firebaseaula/viewmodel/AccessViewModel.kt
@@ -10,34 +10,27 @@ class AccessViewModel : ViewModel() {
 
     var firebaseAuth = Firebase.auth
 
-    var onUserRequestToRegister = MutableLiveData<Boolean>()
+    private var onUserRequestToRegister = MutableLiveData<Boolean>()
     var createUserLiveData : LiveData<Boolean> = onUserRequestToRegister
 
-    var onUserRequestToSignIn = MutableLiveData<Boolean>()
+    private var onUserRequestToSignIn = MutableLiveData<Boolean>()
     var userAuthLiveData : LiveData<Boolean> = onUserRequestToSignIn
 
     fun onCreateUser (email : String , password : String) {
         var registerTask = firebaseAuth.createUserWithEmailAndPassword(email,password)
 
-        registerTask.addOnSuccessListener {
+        registerTask.addOnCompleteListener {
             onUserRequestToRegister.value = registerTask.isSuccessful
         }
 
-        registerTask.addOnFailureListener {
-            onUserRequestToRegister.value = registerTask.isSuccessful
-        }
 
     }
 
     fun onSignIn(email : String , password : String) {
         var authTask = firebaseAuth.signInWithEmailAndPassword(email,password)
 
-        authTask.addOnSuccessListener {
-            onUserRequestToSignIn.value = authTask.isSuccessful
-        }
-
-        authTask.addOnFailureListener {
-            onUserRequestToSignIn.value = authTask.isSuccessful
+        authTask.addOnCompleteListener {
+            onUserRequestToRegister.value = authTask.isSuccessful
         }
 
     }
